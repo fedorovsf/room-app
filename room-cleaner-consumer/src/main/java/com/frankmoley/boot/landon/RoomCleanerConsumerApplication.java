8d7e4a4276a3815bc3700f5610fae87974b1e1d3
Sergei Fedorov <fedorovs@inbox.ru>
2019-01-19 19:09:13 +0300

    adding starting project

---
+++ room-cleaner-consumer/src/main/java/com/frankmoley/boot/landon/RoomCleanerConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.frankmoley.boot.landon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomCleanerConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomCleanerConsumerApplication.class, args);
+    }
+
+}
+
